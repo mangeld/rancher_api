b10a2e96bee05c65cbff400ad60fa15dd0d1a161
Miguel Ángel Durán González <mduran@habitissimo.com>
2016-12-13 13:55:26 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="rancher",
+    version="0.1.1",
+    author="Miguel Ángel Durán González",
+    author_email="hi@mangel.me",
+    url="https://github.com/mangeld/rancher_api",
+    packages=['rancher',],
+    install_requires=[
+        'requests==2.12.1',
+    ],
+)
