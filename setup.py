11eb4351175c539ef99e36e9acded0936f81bd76
Miguel Ángel Durán González <mduran@habitissimo.com>
2016-12-13 15:37:50 +0200

    Bump version to: 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="rancher",
-    version="0.1.1",
+    version="0.1.2",
     author="Miguel Ángel Durán González",
     author_email="hi@mangel.me",
     url="https://github.com/mangeld/rancher_api",
