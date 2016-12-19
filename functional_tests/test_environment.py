4e67773c3994b48c74b49cc172ed8ca44257ae83
Miguel Ángel Durán González <mduran@habitissimo.com>
2016-12-19 11:30:48 +0100

    add test for creating envs with docker-compose

--- functional_tests/test_environment.py
+++ functional_tests/test_environment.py
@@ -5,6 +5,11 @@ from rancher.engine import RequestAdapter
 from rancher.models import RancherEnvironment
 
 
+DOCKER_COMPOSE = """
+nginx:
+    image: nginx:latest
+"""
+
 class TestEnvironmentIsListed:
 
     def test_enviroments_are_listed(self):
@@ -14,9 +19,25 @@ class TestEnvironmentIsListed:
 
         settings.url = "http://rancher.habitissimo.lan:8080/v1"
 
-        env = choice(client.list_envs())
+        env = choice(client.envs)
         assert isinstance(env, RancherEnvironment)
 
+    def test_create_env_with_docker_compose(self):
+        settings = api.ApiSettings()
+        settings.url = "http://rancher.habitissimo.lan:8080/v1"
+        http_client = RequestAdapter()
+        client = api.RancherApi(settings, http_client)
+
+        account = client.get_account("Default")
+        environment = client.create_env(
+            "created-from-python-with-compose",
+            account,
+            docker_compose=DOCKER_COMPOSE
+        )
+        service_count = len(environment.services)
+        environment.remove()
+
+        assert service_count == 1
 
     def test_create_env(self):
         settings = api.ApiSettings()
